a2eadb5ae77215fda6a6940db588164461385f7d
ela336 <ij8319366@gmail.com>
2026-04-18 19:36:51 +0600

    pricing section designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import Navbar from './component/Navbar'
 import Herro from './component/Herro'
 import Stat from './component/Stat'
+import Tools from './component/Tools'
+import Steps from './component/Steps'
+import Footer from './component/Footer'
+
 import './App.css'
+
+const getdata = async()=>{
+  const res=await fetch("/carddata.json");
+
+  return res.json();
+
+}
+
+const datapromise = getdata();
 
 function App() {
   
@@ -12,6 +25,9 @@ function App() {
      <Navbar></Navbar>
      <Herro></Herro>
       <Stat></Stat>
+      <Tools datapromise={datapromise}></Tools>
+      <Steps></Steps>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/component/Tools.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Check } from "lucide-react";
+import { use } from "react";
+
+
+
+
+const Tools = ( {datapromise} ) => {
+    const cards=use(datapromise);
+    console.log(cards);
+    return (
+        <>
+        <div className='text-center mt-22 space-y-4'>
+            <h3 className='font-extrabold text-[48px] '>Premium Digital Tools</h3>
+            <p className='text-[16px] font-normal text-[#627382] text-center' >Choose from our curated collection of premium digital products designed <br /> to boost your productivity and creativity.</p>
+            <div className='flex items-center justify-center gap-6 container mx-auto mt-8'>
+                <button className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white font-bold py-2 px-4 rounded-3xl text-[16px] font-medium'>Products</button>
+                <button className='text-[16px] font-medium'>Cart</button>
+            </div>
+            
+        </div>
+        <div className='grid grid-cols-3 gap-6 w-[80%] container mx-auto mt-12'>
+           {cards.map((card, index) =>
+            <div key={index} className='rounded-3xl border border-[#b5bdc5] p-6 space-y-4'>
+                                    <div className='flex justify-between items-center' >
+                                       <div className='rounded-full border border-[#c8d0d7] flex items-center justify-center w-14 h-14'> <img src={card.image} alt={card.title} width="40px" height="40px" /></div>
+                                        <div className='bg-blue-400 rounded-3xl p-2'>{card.status}</div>
+                                        </div>
+                                        <div>
+                                            <h3 className='text-[24px] font-bold mb-3'>{card.title}</h3>
+                                            <p className='text-[16px] text-[#627382]'>{card.description}</p>
+                                        </div>
+                                        <div className='flex '>
+                                            <h3 className='font-bold'>{card.price}</h3><span className='text-[16px] text-[#627382]'>/{card.period}</span>
+                                            </div>
+            
+                                            <div className="space-y-2">
+  {card.features.map((feature, i) => (
+    <div key={i} className="flex items-center gap-2">
+      <Check className="text-green-500" />
+      <p className="text-[16px] text-[#627382] font-medium">
+        {feature}
+      </p>
+    </div>
+  ))}
+</div>
+                                        <button className='btn btn-primary rounded-3xl w-full'>Buy Now</button>
+                                    
+                                </div>
+           )}
+        </div>
+
+        
+        </>
+    );
+};
+
+export default Tools;
